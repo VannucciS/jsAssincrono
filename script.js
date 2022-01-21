5d3584d16ba7da4bb2d4428f3a66f95104acbf5c
vannucci <homelete@gmail.com>
2022-01-21 14:50:31 -0300

    exericio completo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ Nesta atividade, vamos criar uma página que carrega fotos aleatórias de gatinh
 Utilize a API https://thatcopy.pw/catapi/rest para fazer as chamadas com o método fetch();
 Utilize seus conhecimentos na manipulação do DOM para criar a imagem e ativar o evento de clique do botão!
 */
-
+/*
 const url = "https://thatcopy.pw/catapi/rest"
 const catBtn = document.getElementById('catBtn')
 const catImg = document.getElementById('catImg')
@@ -20,4 +20,36 @@ function getCatImg() {
 
         })
 }
+*/
 
+const BASE_URL = "https://thatcopy.pw/catapi/rest/"
+const catBtn = document.getElementById("catBtn");
+const catImg = document.getElementById("catImg");
+
+/*
+const getCats = async() => {
+try {
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+
+    return  json.webpurl;
+    } catch (e) {
+    console.log(e.message);
+                }
+}       
+*/
+const getCats = async() => {
+    const data = await fetch (BASE_URL)
+        .then((res)=> res.json())
+        .catch((e)=> console.log(e));
+
+    return data.webpurl;
+}
+
+const loadImg = async () => {
+    catImg.src = await getCats();
+}
+
+catBtn.addEventListener('click', loadImg)
+
+loadImg();
